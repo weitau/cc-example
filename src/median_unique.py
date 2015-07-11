1339e821b177db316ead30a9b6e61db3e474e280
Robert Tau <robert@snazzle.com>
2015-07-10 21:24:45 -0400

    final versions

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,24 +1,25 @@
 from sys import argv
 script, first_parm, second_parm = argv
 
-
 inputfile = open(first_parm)
 outfile = open(second_parm, 'w')
 
-# print "Here's your file %r:" % first_parm
+def xDupwords(words):
+    for a, b in zip(words, words[1:]) :
+        if a == b:
+            words.remove(b)
+    return words    
 
-line_cnt = 0.0      #counter of lines read
+line_cnt = 0.0      #counter of tweets read
 tweetword_cnt = 0   #count of unique words in tweet
 sum_words = 0.0     #running count of unique words found in tweets
+
 for lines in inputfile:
     if len(lines) > 1:   #ensure tweet has at least 1 character other than EOL
         line_cnt += 1
         listx = lines.split()
         listx.sort()     #sort the words in the tweet for the next operation
-#        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-#        print listx
         xDupwords(listx) #remove consecutive repeating words
-#        print listx
         tweetword_cnt = len(listx)
         sum_words = sum_words + tweetword_cnt
         outfile.write(str("{:.2f}".format(sum_words/line_cnt)+"\n"))
@@ -26,12 +27,3 @@ for lines in inputfile:
 inputfile.close()
 outfile.close()
 
-# print "The script is called:", script
-# print "Your first variable is:", first_parm
-# print "Your second variable is:", second_parm
-
-
-
-
-
-# example of program that calculates the median number of unique words per tweet.
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,1 +1,29 @@
-# example of program that calculates the total number of times each word has been tweeted.
+from sys import argv
+script, first_parm, second_parm = argv
+
+
+inputfile = open(first_parm)
+outfile = open(second_parm, 'w')
+
+wordlist = {}  #init dictionary to hold list of words read as key and value as num times seen
+
+def tally(words):
+    for one_word in words:
+        if one_word in wordlist:
+            wordlist[one_word] += 1
+        else:
+            wordlist[one_word] = 1
+        
+
+for lines in inputfile:  # read one tweet at a time
+    if len(lines) > 1:   # ensure tweet has at least 1 character other than EOL
+        listx = lines.split()
+        tally(listx)     # count the instances of each word in the single tweet
+
+for word in sorted(wordlist):
+    outfile.write("%20s \t %d \n" % (word.ljust(20), wordlist[word]) )
+
+    
+inputfile.close()
+outfile.close()
+
